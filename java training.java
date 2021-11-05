91a52f5b9195327abef60a4a358d27762c3c5c27
Augusta <augusta.ezike@yahoo.com>
2021-11-05 03:14:26 -0600

    exception

--- java training.java
+++ java training.java
@@ -795,3 +795,39 @@ public class Aah extends DiceRoller{
 		System.out.println("This is the way we move");
 	}
 }
+
+
+//EXCEPTIONS
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main (String[] args) {
+		
+		//EXCEPTIONS try catch block and finally
+		Scanner scanner = new Scanner(System.in);
+		try {			
+			System.out.println("Enter a whole number to divide");
+			int x = scanner.nextInt();
+			
+			System.out.println("Enter a whole number to divide by:");
+			int y = scanner.nextInt();
+			
+			int z = x/y;
+			System.out.println(z);			
+		}
+		catch (ArithmeticException e) {
+			System.out.println("the math is not mathing, try again");
+		}catch (InputMismatchException e) {
+			System.out.println("Cannot divde by a non number, please enter a number");			
+		}catch (Exception e) { //catches all exception. usually good practise not to use this and to catch exceptions individually
+			System.out.println("Something went wrong, please try again");
+		}
+		finally {
+			scanner.close();
+			System.out.println("This will always print ");
+		}		
+	}	
+}
+ 
