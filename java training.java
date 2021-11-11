d01f8a942a699960766e719fffbf07db552e1aec
Augusta <augusta.ezike@yahoo.com>
2021-11-11 03:05:52 -0700

    audio files

--- java training.java
+++ java training.java
@@ -1044,3 +1044,46 @@ public class Main {
 	}	
 }
 
+
+//WORKING WITH AUDIO FILES
+//Creating a basic audio player (works only with wav. files and not mp3, you will need to convert all files to wav.)
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+import javax.sound.sampled.*;
+
+public class Main {
+
+	public static void main (String[] args) throws UnsupportedAudioFileException, IOException, LineUnavailableException {
+		Scanner scanner = new Scanner(System.in);
+		File file = new File("it-is-so.wav"); // copy music file into the src directly. it should be on the same level with the default package 
+		AudioInputStream audioStream = AudioSystem.getAudioInputStream(file);
+		Clip clip = AudioSystem.getClip();
+		clip.open(audioStream);
+				
+		String response = " ";
+		
+		while (!response.equals("Q")) {
+			System.out.println("P = Play, S = Stop, R = Reset, Q = Quit");
+			System.out.println("Enter your choice: ");
+			
+			response = scanner.next();
+			response = response.toUpperCase();
+			
+			switch(response) {
+			case ("P"): clip.start();
+			break;
+			case ("S"): clip.stop();
+			break;
+			case ("R"): clip.setMicrosecondPosition(0); //sets the reset to what time in micro seconds
+			break;
+			case ("Q"): clip.close();
+			break;
+			default: System.out.println("Not a valid response");
+			}
+		}
+		System.out.println("Byee");
+	}	
+}
+ 
