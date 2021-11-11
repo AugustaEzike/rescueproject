db6d44d42b083beba47b7ddfe3d08144d772c64f
Augusta <augusta.ezike@yahoo.com>
2021-11-11 12:27:37 -0700

    GUI buttons

--- java training.java
+++ java training.java
@@ -1163,4 +1163,55 @@ public class Main {
 		
 	}	
 }
+
+//PANELS
+import java.awt.Color;
+
+import javax.swing.BorderFactory;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.border.Border;
+
+import java.awt.Font;
+
+public class Main {
+
+	public static void main (String[] args) {
+		
+		//JPANEL - a GUI component that functions as a container to hold other componets
+		ImageIcon icon = new ImageIcon("cart.png");
+		
+		JLabel label = new JLabel();
+		label.setText("Hi");
+		label.setIcon(icon);
+		label.setVerticalAlignment(JLabel.TOP);
+		label.setHorizontalAlignment(JLabel.RIGHT);
+		
+		JPanel redPanel = new JPanel();
+		redPanel.setBackground(Color.red);
+		redPanel.setBounds(0, 0, 250, 250);
+		
+		JPanel bluePanel = new JPanel();
+		bluePanel.setBackground(Color.blue);
+		bluePanel.setBounds(250, 0, 250, 250);
+		
+		JPanel greenPanel = new JPanel();
+		greenPanel.setBackground(Color.green);
+		greenPanel.setBounds(0, 250, 500, 250);
+		
+		JFrame frame = new JFrame();
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(500,500);
+		frame.setLayout(null);
+		frame.setVisible(true);
+		frame.add(redPanel); 	//add panel to frame
+		frame.add(bluePanel);
+		frame.add(greenPanel);
+		redPanel.add(label);
+		
+	}	
+}
+ 
  
