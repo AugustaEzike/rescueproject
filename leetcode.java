3be13c61dd102877670222fbcd0f3a9aa7652055
Augusta <augusta.ezike@yahoo.com>
2021-12-18 22:07:59 -0700

    solved some challenges

--- leetcode.java
+++ leetcode.java
@@ -1,1 +1,18 @@
 //not really sure about anything right now
+hello world
+hello world
+hello world
+hello world
+hello world
+hello world
+hello world
+hello world
+hello world
+hello world
+hello world
+hello world
+hello world
+hello world
+hello world
+hello world
+hello world
